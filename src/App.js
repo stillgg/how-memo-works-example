d229e9fe0097a3697d578ff12d72d1fbaf16f094
Egor <still2095@gmail.com>
2024-04-12 15:03:04 +0300

    add example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback, memo } from "react";
 
-function App() {
+const Foo = memo(({ arr, setArr, increment }) => {
+  console.log("render foo");
+
+  useEffect(() => {
+    console.log("effect foo");
+
+    setTimeout(() => {
+      setArr([{ value: 1 }, { value: 2 }]);
+    }, 500);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={increment}>Foo</button>
+
+      <br />
+
+      {arr.map((item) => (
+        <div key={item.value}>{item.value}</div>
+      ))}
+    </div>
+  );
+});
+
+const Bar = ({ counter, increment, decrement }) => {
+  console.log("render bar");
+
+  return (
+    <div>
+      <h1>Bar</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <h2>{counter}</h2>
+    </div>
+  );
+};
+
+export default function App() {
+  const [arr, setArr] = useState([]);
+
+  const [counter, setCounter] = useState(1);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Foo
+        arr={arr}
+        setArr={setArr}
+      />
+      <Bar counter={counter} increment={increment} decrement={decrement} />
     </div>
   );
 }
-
-export default App;
